1e240c24e453d5704ae876d6987d64f51b079a28
Dawnaaaa <117069128+Dawnaaaa@users.noreply.github.com>
2022-10-31 16:24:31 +0800

    Update mubu.js

--- mubu.js
+++ mubu.js
@@ -8,14 +8,14 @@
 群1077223830
 *******************************
 [rewrite_local]
-^http[s]?:\/\/api2.mubu.com\/v3\/api\/user\/current_user.+$ url script-response-body mubu.js
+^http[s]?:\/\/api2.mubu.com\/v3\/api\/user\/current_user.+$ url script-response-body https://raw.githubusercontent.com/Dawnaaaa/Qx/main/mubu.js
 [mitm] 
 hostname = *.mubu.*
 *******************************
 Surge
 
 [Script]
-^http[s]?:\/\/api2.mubu.com\/v3\/api\/user\/current_user.+$ requires-body=1,max-size=0,script-path=mubu.js
+^http[s]?:\/\/api2.mubu.com\/v3\/api\/user\/current_user.+$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Dawnaaaa/Qx/main/mubu.js
 
 [MITM]
 hostname = *.mubu.*
