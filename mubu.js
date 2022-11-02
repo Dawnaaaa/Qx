b2fa004beaadaf3be9a4617a81c56b53f3d37816
Dawnaaaa <117069128+Dawnaaaa@users.noreply.github.com>
2022-11-02 13:27:26 +0800

    Update mubu.js

--- mubu.js
+++ mubu.js
@@ -1,11 +1,7 @@
 /*******************************
-  公众号:木木IOS分享
-关注了解更多新科技！！！
-幕布（终身会员）
 脚本名称:幕布（终身会员）
 使用声明：️此脚本仅供学习与交流，
         请勿转载与贩卖！️️️
-群1077223830
 *******************************
 [rewrite_local]
 ^https:\/\/api2.mubu.com\/v3\/api\/user/* url script-response-body https://raw.githubusercontent.com/Dawnaaaa/Qx/main/mubu.js
