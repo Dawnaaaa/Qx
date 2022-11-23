d73bcd2efa4da51da2f6b420580b25c9ff75dc53
Dawnaaaa <117069128+Dawnaaaa@users.noreply.github.com>
2022-11-23 10:27:52 +0800

    Create Dawn.js

---
+++ Dawn.js
@@ -0,0 +1,12 @@
+/*
+#已添加脚本：幕布,cubox
+*/
+
+hostname =cubox.pro,api2.mubu.com
+
+#幕布-终身会员
+^https:\/\/api2.mubu.com\/v3\/api\/user/* url script-response-body https://raw.githubusercontent.com/Dawnaaaa/Qx/main/mubu.js
+
+#cubox-收藏阅读+解锁VIP
+^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/Dawnaaaa/Qx/main/cubox.js
+
