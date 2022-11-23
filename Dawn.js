3aeacb5b9b460a9ac86004c91b4cc55c2daa9183
Dawnaaaa <117069128+Dawnaaaa@users.noreply.github.com>
2022-11-23 12:23:22 +0800

    Update Dawn.js

--- Dawn.js
+++ Dawn.js
@@ -8,5 +8,5 @@ hostname =cubox.pro,api2.mubu.com
 ^https:\/\/api2.mubu.com\/v3\/api\/user/* url script-response-body https://raw.githubusercontent.com/Dawnaaaa/Qx/main/mubu.js
 
 #cubox-收藏阅读+解锁VIP
-^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/Dawnaaaa/Qx/main/cubox.js
+#^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/Dawnaaaa/Qx/main/cubox.js
 
